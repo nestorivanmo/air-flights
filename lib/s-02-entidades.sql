3f31de0c03a2d10993dc6830b2f85678bd0f4787
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-17 00:04:47 -0500

    added unique and check constraints

--- lib/s-02-entidades.sql
+++ lib/s-02-entidades.sql
@@ -9,14 +9,14 @@ whenever sqlerror exit;
 --
 prompt creando tabla avion;
 create table avion(
-    id_avion                    number(10) not null,
+    id_avion                    number(10)  not null,
     matricula                   varchar2(10) not null,
     modelo_avion                varchar2(50) not null,
     documento_especificaciones  binary(2000) not null,
     es_carga                    number(1,0) not null,
     es_comercial                number(1,0) not null,
-
-    constraint avion_pk primary key(id_avion)
+    constraint avion_pk primary key(id_avion),
+    constraint av_matricula_uk unique(matricula)
 );
 
 --
@@ -28,10 +28,12 @@ create table avion_comercial(
     num_ordinarios              number(3,0) not null,
     num_vip                     number(3,0) not null,
     num_discapacitados          number(3,0) not null,
-
     constraint avion_comercial_pk primary key (id_avion),
-    constraint avion_comercial_id_avion_fk foreign key(id_avion) 
-    references avion(id_avion)
+    constraint avc_id_avion_fk foreign key(id_avion) 
+    references avion(id_avion),
+    constraint avco_num_ordinarios_chk check (num_ordinarios >= 0),
+    constraint avco_num_vip_chk check (num_vip >= 0),
+    constraint avco_num_discapacitados_chk check (num_discapacitados >= 0)
 );
 
 --
@@ -44,10 +46,11 @@ create table avion_carga(
     capacidad_carga             number(5,2) not null,
     alto                        number(3,2) not null,
     ancho                       number(4,2) not null,
-    volumen                     number(6,2) as (alto*ancho) virtual,
-
+    volumen                     number(6,2) as (round(alto*ancho, 2)) virtual,
+    capacidad_libras            number(5,2) as (round(capacidad_carga * 2.2046 
+        * 1000),2) virtual,
     constraint avion_carga_pk primary key (id_avion),
-    constraint avion_carga_id_avion_fk foreign key(id_avion) 
+    constraint avcarga_id_avion_fk foreign key(id_avion) 
     references avion(id_avion)
 );
 
@@ -62,9 +65,10 @@ create table pasajero(
     apellido_materno            varchar2(50) null,
     email                       varchar2(100) null,
     fecha_nacimiento            date not null,
-    curp varchar2(13)            not null,
-
-    constraint pasajero_pk primary key (id_pasajero)
+    curp                        varchar2(13) not null,
+    constraint pasajero_pk primary key (id_pasajero),
+    constraint pa_curp_uk unique(curp), 
+    constraint pa_email_uk unique(email)
 );
 
 --
@@ -78,8 +82,9 @@ create table aeropuerto(
     latitud                     number(9,7) not null,
     longitud                    number(9,7) not null,
     es_activo                   number(1,0) not null,
-
-    constraint aeropuerto_pk primary key (id_aeropuerto)
+    constraint aeropuerto_pk primary key (id_aeropuerto),
+    constraint ae_clave_uk unique(clave),
+    constraint ae_es_activo_chk check (es_activo in (0, 1))
 );
 
 --
@@ -92,8 +97,9 @@ create table puesto_asignado(
     nombre                      varchar2(50) not null,
     descripcion                 varchar2(300) not null,
     sueldo_mensual              number(9,2) not null,
-
-    constraint puesto_asignado_pk  primary key (id_puesto_asignado)
+    constraint puesto_asignado_pk  primary key (id_puesto_asignado),
+    constraint pu_clave_uk unique(clave),
+    constraint pu_sueldo_mensual_chk check (sueldo_mensual >= 0)
 );
 
 --
@@ -111,12 +117,14 @@ create table empleado(
     puntos                      number(3,0) not null,
     id_jefe                     number(10,0) null,
     id_puesto_asignado          number(10,0) not null,
-
     constraint empleado_pk primary key (id_empleado),
-    constraint empleado_id_jefe_fk foreign key(id_jefe) 
+    constraint em_id_jefe_fk foreign key(id_jefe) 
     references empleado(id_empleado),
-    constraint empleado_id_puesto_asignado_fk foreign key(id_puesto_asignado) 
-    references puesto_asignado(id_puesto_asignado)
+    constraint em_id_puesto_asignado_fk foreign key(id_puesto_asignado) 
+    references puesto_asignado(id_puesto_asignado),
+    constraint em_rfc_uk unique(rfc),
+    constraint em_curp_uk unique(curp),
+    constraint em_puntos_chk check (puntos >= 0 and puntos <= 100)
 );
 
 --
@@ -125,11 +133,10 @@ create table empleado(
 prompt creando tabla url_trayectoria;
 create table url_trayectoria(
     id_url_trayectoria          number(10) not null,
-    url                         varchar2(30) not null,
+    url_trayectoria             varchar2(30) not null,
     id_empleado                 number(10) not null,
-
     constraint url_trayectoria_pk primary key (id_url_trayectoria),
-    constraint url_trayectoria_id_url_trayectoria_fk foreign key(id_empleado)
+    constraint ut_id_url_trayectoria_fk foreign key(id_empleado)
     references empleado(id_empleado)
 );
 
@@ -142,8 +149,8 @@ create table tipo_paquete(
     clave                       varchar2(5) not null,
     descripcion                 varchar2(50) not null,
     indicaciones_generales      varchar2(30) not null,
-
     constraint tipo_paquete_pk primary key (id_tipo_paquete)
+    constraint tp_clave_uk unique(clave)
 );
 
 --
@@ -153,13 +160,14 @@ prompt creando tabla paquete;
 create table paquete(
     id_paquete                  number(10,0) not null,
     folio                       varchar2(18) not null,
-    peso                        number(8,2) not null,
-    peso_libras                 number(8,2) not null,
+    peso                        number(8,2) not null, --en kg
+    peso_libras                 number(8,2) as (peso * 2.2046) virtual,
     id_tipo_paquete             number(10,0) not null,
-
     constraint paquete_pk primary key (id_paquete),
-    constraint paquete_id_tipo_paquete_fk foreign key(id_tipo_paquete) 
-    references tipo_paquete(id_tipo_paquete)
+    constraint pq_id_tipo_paquete_fk foreign key(id_tipo_paquete) 
+    references tipo_paquete(id_tipo_paquete),
+    constraint pq_folio_uk unique(folio)
+    constraint pq_peso_chk check (peso > 0)
 );
 
 --
@@ -168,14 +176,13 @@ create table paquete(
 prompt creando tabla pase_abordar;
 create table pase_abordar(
     id_pase_abordar             number(10,0) not null,
-    folio                       varchar2(4) not null,
+    folio                       varchar2(10) not null,
     fecha                       date not null default sysdate,
-    asiento                     varchar2(5) not null,
     id_pasajero                 number(10,0) not null,
-
     constraint pase_abordar_pk primary key(id_pase_abordar),
-    constraint pase_abordar_id_pasajero_fk foreign key(id_pasajero)
-    references pasajero(id_pasajero)
+    constraint pab_id_pasajero_fk foreign key(id_pasajero)
+    references pasajero(id_pasajero),
+    constraint pab_folio_uk unique(folio)
 );
 
 --
@@ -187,10 +194,15 @@ create table equipaje(
     numero                      number(1,0) not null,
     peso                        number(4,2) not null,
     id_pasajero                 number(10,0) not null,
-
+    id_vuelo                    number(10,0) not null,
     constraint equipaje_pk primary key(id_equipaje),
-    constraint equipaje_id_pasajero_fk foreign key(id_pasajero)
-    references pasajero(id_pasajero)
+    constraint eq_id_pasajero_fk foreign key(id_pasajero)
+    references pasajero(id_pasajero),
+    constraint eq_id_vuelo_fk foreign key (id_vuelo)
+    references vuelo(id_vuelo),
+    constraint eq_peso_chk check (peso > 0),
+    constraint eq_numero_chk check (numero > 0),
+    constraint eq_numero_uk unique (numero, id_pasajero, id_vuelo)
 );
 
 --
@@ -201,8 +213,8 @@ create table status_vuelo(
     id_status_vuelo             number(10,0) not null,
     clave                       varchar2(5) not null,
     descripcion                 varchar2(50) not null,
-
-    constraint status_vuelo primary key(id_status_vuelo)
+    constraint status_vuelo_pk primary key(id_status_vuelo),
+    constraint sv_clave_uk unique(clave)
 );
 
 --
@@ -217,20 +229,47 @@ create table vuelo(
     fecha_llegada               date not null,
     numero_vuelo                varchar2(8) not null,
     sala_abordar                varchar2(5) not null,
-    distancia                   number(7,2) not null,
-    duracion                    number(2,0) not null,
+    distancia                   number(7,2) as (
+        select round(
+            sqrt(
+                power((d.latitud*111.13655555555556)
+                - (o.latitud*111.13655555555556),2)+
+                power((d.longitud*111.13655555555556)
+                - (o.longitud*111.13655555555556),2) 
+            ), 3) as distancia
+        from aeropuerto o, aeropuerto d
+        where o.id = id_aeropuerto_origen
+        and d.id = id_aeropuerto_destino
+    ) virtual,
+    duracion                    varchar2(2,0) as (
+        to_char(fecha_llegada - fecha_salida, 'HH24:MI:SS')
+    ) virtual,
     id_status_vuelo             number(10,0) not null,
     id_avion                    number(10,0) not null,
-
     constraint vuelo_pk primary key (id_vuelo),
-    constraint vuelo_id_aeropuerto_origen_fk foreign key(id_aeropuerto_origen)
+    constraint vu_id_aeropuerto_origen_fk foreign key(id_aeropuerto_origen)
     references aeropuerto(id_aeropuerto),
-    constraint vuelo_id_aeropuerto_destino_fk foreign key(id_aeropuerto_destino)
+    constraint vu_id_aeropuerto_destino_fk foreign key(id_aeropuerto_destino)
     references aeropuerto(id_aeropuerto),
-    constraint vuelo_id_status_vuelo_fk foreign key(id_status_vuelo)
+    constraint vu_id_status_vuelo_fk foreign key(id_status_vuelo)
     references status_vuelo(id_status_vuelo),
-    constraint vuelo_id_avion_fk foreign key(id_avion)
+    constraint vu_id_avion_fk foreign key(id_avion)
     references avion(id_avion)
+);
+
+---
+--- Table: LISTA_UBICACIONES
+---
+prompt creando table lista_ubicaciones;
+create table lista_ubicaciones(
+    id_lista_ubicaciones        number(10,0) not null,
+    latitud                     number(10,7) not null,
+    longitud                    number(10,7) not null,
+    fecha_hora                  date not null,
+    id_vuelo                    number(10,0) not null,
+    constraint lista_ubicaciones_pk primary key(id_lista_ubicaciones),
+    constraint lu_id_vuelo_fk foreign key(id_vuelo)
+    references vuelo(id_vuelo)
 );
 
 --
@@ -242,11 +281,10 @@ create table historico_status_vuelo(
     fecha                       date not null default sysdate,
     id_status_vuelo             number(10,0) not null,
     id_vuelo                    number(10,0) not null,
-
-    constraint historico_status_vuelo primary key(id_historico_status_vuelo),
-    constraint historico_status_vuelo_id_status_vuelo_fk 
-    foreign key(id_status_vuelo) references status_vuelo(id_status_vuelo),
-    constraint historico_status_vuelo_id_vuelo_fk foreign key(id_vuelo)
+    constraint historico_status_vuelo_pk primary key(id_historico_status_vuelo),
+    constraint hsv_vuelo_id_status_vuelo_fk foreign key(id_status_vuelo) 
+    references status_vuelo(id_status_vuelo),
+    constraint hsv_id_vuelo_fk foreign key(id_vuelo)
     references vuelo(id_vuelo)
 );
 
@@ -258,12 +296,12 @@ create table lista_paquetes(
     id_lista_paquetes           number(10,0) not null,
     id_vuelo                    number(10,0) not null,
     id_paquete                  number(10,0) not null,
-
     constraint lista_paquetes_pk primary key(id_lista_paquetes),
-    constraint lista_paquetes_id_vuelo_fk foreign key(id_vuelo)
+    constraint lpq_id_vuelo_fk foreign key(id_vuelo)
     references vuelo(id_vuelo),
-    constraint lista_paquetes_id_paquete_fk foreign key(id_paquete)
-    references paquete(id_paquete)
+    constraint lpq_id_paquete_fk foreign key(id_paquete)
+    references paquete(id_paquete),
+    constraint lpq_id_paquete_uk unique (id_paquete, id_vuelo)
 );
 
 --
@@ -275,13 +313,14 @@ create table lista_pasajeros(
     id_vuelo                    number(10,0) not null,
     id_pase_abordar             number(10,0) not null,
     pasajero_presente           number(1,0) not null,
-    atencion_especial           varchar2(300) not null
-
+    atencion_especial           varchar2(300) not null,
+    asiento                     varchar2(5) not null,
     constraint lista_pasajeros_pk primary key(id_lista_pasajeros),
-    constraint lista_pasajeros_id_vuelo_fk foreign key(id_vuelo)
+    constraint lpa_id_vuelo_fk foreign key(id_vuelo)
     references vuelo(id_vuelo),
-    constraint lista_pasajeros_id_pase_abordar_fk foreign key(id_pase_abordar)
-    references pase_abordar(id_pase_abordar)
+    constraint lpa_id_pase_abordar_fk foreign key(id_pase_abordar)
+    references pase_abordar(id_pase_abordar),
+    constraint lpa_id_pase_abordar_uk unique (id_pase_abordar, id_vuelo)
 );
 
 --
@@ -292,27 +331,9 @@ create table tripulacion(
     id_tripulacion              number(10,0) not null,
     id_vuelo                    number(10,0) not null,
     id_empleado                 number(10,0) not null,
-
     constraint tripulacion_pk primary key(tripulacion_id),
-    constraint tripulacion_id_vuelo_fk foreign key(id_vuelo)
+    constraint tr_id_vuelo_fk foreign key(id_vuelo)
     references vuelo(id_vuelo),
-    constraint tripulacion_id_empleado_fk foreign key(id_empleado)
+    constraint tr_id_empleado_fk foreign key(id_empleado)
     references empleado(id_empleado)
-);
-
---
--- Table: T_UBICACION
---
-prompt creando tabla t_ubicacion;
-create table t_ubicacion(
-    id_ubicacion                number(10,0) not null,
-    latitud                     number(10,7) not null,
-    longitud                    number(10,7) not null,
-    fecha                       date not null,
-    tiempo_restante             date not null,
-    id_avion                    number(10,0) not null,
-
-    constraint t_ubicacion_pk primary key(id_ubicacion),
-    constraint t_ubicacion_id_avion_fk foreign key(id_avion)
-    references avion(id_avion)
 );
