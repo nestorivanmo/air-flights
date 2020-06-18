a4b6d911a41d82d39eeeddac508003cddaacdbc3
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-18 12:18:22 -0500

    added initial airplane data load

--- lib/s-09-carga-inicial.sql
+++ lib/s-09-carga-inicial.sql
@@ -1,3 +1,29 @@
 --@Autor(es):       Hector Espino Rojas, Néstor Martínez Ostoa
 --@Fecha creación:  15/06/2020
---@Descripción:     Carga inicial de los datos. 
+--@Descripción:     Carga inicial de los datos.
+
+--
+-- AVION / AVION_COMERCIAL / AVION_CARGA
+--
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, '2l-h2927', 'charlie', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, '6y-b3890', 'juliett', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'r7-p0520', 'uniform', false, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, '2h-e5506', 'whiskey', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'p4-w7954', 'india', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'l3-v2417', 'india', false, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'y0-l0477', 'zulu', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'nx-i6658', 'oscar', false, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'pq-e6405', 'foxtrot', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'bl-o9149', 'hotel', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'w8-f2340', 'oscar', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, '9g-q8152', 'zulu', false, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'tu-d8916', 'oscar', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, '4y-y5547', 'golf', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'pq-t0192', 'quebec', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'fi-c8108', 'zulu', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'et-p3764', 'romeo', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'wn-o8869', 'tango', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'wo-j9685', 'oscar', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'gm-s1719', 'mike', true, true);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'e2-d5347', 'echo', true, false);
+insert into avion (id_avion, matricula, modelo_avion, es_carga, es_comercial) values (avion_seq.nextval, 'r8-r5725', 'uniform', false, true);
