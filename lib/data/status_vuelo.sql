7f6dea0673c9b92c44e9ad044cb07a42d89eb342
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-18 21:07:43 -0500

    updated data

--- lib/data/status_vuelo.sql
+++ lib/data/status_vuelo.sql
@@ -1,7 +1,7 @@
 insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'PROGR', 'Programado');
 insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'ABORD', 'Abordando');
 insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'ATEMP', 'A Tiempo');
-insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'PROGR', 'En progreso');
+insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'VOLAN', 'En vuelo');
 insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'DEMOR', 'Demorado');
 insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'CANCE', 'Cancelado');
 insert into status_vuelo (id_status_vuelo, clave, descripcion) values (status_vuelo_seq.nextval, 'ATERR', 'Aterrizó');
