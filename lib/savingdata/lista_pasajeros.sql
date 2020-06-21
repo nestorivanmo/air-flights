a2854ac93a2ddac64be8d6d272a74770bf6f8bf6
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-21 15:46:09 -0500

    fixed issue

--- lib/savingdata/lista_pasajeros.sql
+++ lib/savingdata/lista_pasajeros.sql
@@ -10,14 +10,19 @@ cursor cur_aviones_comerciales is
 v_counter number;
 v_presente number;
 v_insert_stmt varchar2 (400);
+v_num_asiento number;
+v_lett_asiento varchar2(1);
 begin
   v_counter := 1;
   for r in cur_aviones_comerciales loop
     for c in 1 .. 10 loop
       v_presente := dbms_random.value(0,1);
-      v_insert_stmt := 'insert into lista_pasajeros(:1, :2, :3, :4, :5, :6)';
+      v_lett_asiento := dbms_random.string('U', 1);
+      v_num_asiento := dbms_random.value(1,10);
+      v_insert_stmt := 'insert into lista_pasajeros values(:1, :2, :3, :4, :5, :6)';
       execute immediate v_insert_stmt using lista_pasajeros_seq.nextval,
-        r.id_vuelo, v_counter, 'A-23', v_presente, 'lorem ipsum';
+        r.id_vuelo, v_counter, v_lett_asiento || '-' || v_num_asiento, v_presente,
+        'lorem ipsum';
       v_counter := v_counter + 1;
     end loop;
   end loop;
