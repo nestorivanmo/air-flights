07f741c33b79ef0015399647109755b6765c12a5
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-20 23:43:00 -0500

    lista paquetes

--- lib/s-09-carga-inicial-saving.sql
+++ lib/s-09-carga-inicial-saving.sql
@@ -3,13 +3,13 @@
 --@Descripción:     Carga inicial de los datos.
 
 whenever sqlerror exit;
-start savingdata/aviones_combinados.sql; --1-300 (hib) 301-600 (carg) 601-900 (com)
-start savingdata/puesto_asignado.sql; --9
-start savingdata/empleados.sql; -- 2050
-start savingdata/url_trayectoria.sql;
-start savingdata/tipo_paquete.sql; --1 -> 10
-start savingdata/paquete.sql; --> 11k
-start savingdata/pasajero.sql; --> 20k
+start savingdata/aviones_combinados.sql  --1-300 (hib) 301-600 (carg) 601-900 (com)
+start savingdata/puesto_asignado.sql  --9
+start savingdata/empleados.sql  -- 2050
+start savingdata/url_trayectoria.sql 
+start savingdata/tipo_paquete.sql  --1 -> 10
+start savingdata/paquete.sql  --> 11k
+start savingdata/pasajero.sql  --> 20k
 start savingdata/pase_abordar.sql --> 25k
 start savingdata/status_vuelo.sql --> 5
 ---vuelos
@@ -20,6 +20,7 @@ Posteriormente, el histórico tomará cada uno de estos vuelos y dependiendo
 de sus status, insertará dentro del histórico con fechas menores y un status
 menor.
 */
-start savingdata/vuelo.sql; -- 3600
+start savingdata/vuelo.sql -- 3600
 start savingdata/historico_status_vuelo.sql --14000
 start savingdata/lista_ubicaciones.sql --20k (únicamente hay registros para los vuelos del 2016)
+start savingdata/lista_paquetes.sql --
---
+++ lib/savingdata/lista_paquetes.sql
@@ -0,0 +1,37 @@
+set serveroutput on;
+
+create or replace sp_insert_into_lista_paquetes(
+  p_id_vuelo in number,
+  p_id_paquete in number
+) is 
+  v_sql_stmt varchar2;
+begin
+  v_sql_stmt := '
+    insert into lista_paquetes(id_lista_paquetes, id_vuelo, id_paquete)
+    values(lista_paquetes_seq.nextval, :1, :2)
+  ';
+  execute immediate v_sql_stmt using p_id_vuelo, p_id_paquete;
+end;
+
+declare
+cursor cur_vuelos_carga is
+  select * from vuelo
+  from vuelo
+  where id_avion = (
+    select id_avion
+    from avion
+    where es_carga = 1
+  );
+v_id_paquete paquete.id_paquete%type;
+v_counter number;
+begin
+  v_counter := 1;
+  for r in cur_vuelos_carga loop
+    for c in 1 .. 3 loop
+      sp_insert_into_lista_paquetes(r.id_vuelo, v_counter);
+      v_counter := v_counter + 1;
+    end loop;
+  end loop;
+end;
+/
+show errors;
