168f9692016c07498eb3ba7a3b692dabc817943c
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-20 23:16:27 -0500

    minor update

--- lib/s-09-carga-inicial-saving.sql
+++ lib/s-09-carga-inicial-saving.sql
@@ -1,12 +1,12 @@
-start saving-data/aviones_combinados.sql; --1-300 (hib) 301-600 (carg) 601-900 (com)
-start saving-data/puesto_asignado.sql; --9
-start saving-data/empleados.sql; -- 2050
-start saving-data/url_trayectoria.sql;
-start saving-data/tipo_paquete.sql; --1 -> 10
-start saving-data/paquete.sql; --> 11k
-start saving-data/pasajero.sql; --> 20k
-start saving-data/pase_abordar.sql --> 25k
-start saving-data/status_vuelo.sql --> 5
+start savingdata/aviones_combinados.sql; --1-300 (hib) 301-600 (carg) 601-900 (com)
+start savingdata/puesto_asignado.sql; --9
+start savingdata/empleados.sql; -- 2050
+start savingdata/url_trayectoria.sql;
+start savingdata/tipo_paquete.sql; --1 -> 10
+start savingdata/paquete.sql; --> 11k
+start savingdata/pasajero.sql; --> 20k
+start savingdata/pase_abordar.sql --> 25k
+start savingdata/status_vuelo.sql --> 5
 ---vuelos
 /*
 generar 900 vuelos por cada año entre el 2016 y 2019 todos con un status vuelo
@@ -15,5 +15,6 @@ Posteriormente, el histórico tomará cada uno de estos vuelos y dependiendo
 de sus status, insertará dentro del histórico con fechas menores y un status
 menor.
 */
-start saving-data/vuelo.sql; -- 3600
-start saving-data/historico_status_vuelo.sql --14000
+start savingdata/vuelo.sql; -- 3600
+start savingdata/historico_status_vuelo.sql --14000
+start savingdata/lista_ubicaciones.sql --20k (únicamente hay registros para los vuelos del 2016)
---
+++ lib/savingdata/puesto_asignado.sql
@@ -0,0 +1,9 @@
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'DIRGR', 'Director Geneneral', 'Administra la aerolínea', 150000.75);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'GERPIL', 'Gerente Pilotos', 'Administra a los pilotos', 120000.50);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'GERSOB', 'Gerente Sobrecargos', 'Administra a los sobrecargos', 110000.50);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'GERTEC', 'Gerente Tecnicos', 'Administra a los técnicos', 100000.50);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'PIL', 'Piloto', 'Dirige los aviones en vuelos.', 90000.43);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'COP', 'Copiloto', 'Ayuda al copiloto en los vuelos.', 75000.38);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'TEC', 'Tecnico', 'Encargado de administrar las bodegas.', 45000.34);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'JEFSOB', 'Jefe Sobrecargos', 'Administra a los sobrecargos en vuelos comerciales.', 50004.52);
+insert into puesto_asignado (id_puesto_asignado, clave, nombre, descripcion, sueldo) values (puesto_asignado_seq.nextval, 'SOB', 'Sobrecargo', 'Atiende a los pasajeros.', 45000.34);
