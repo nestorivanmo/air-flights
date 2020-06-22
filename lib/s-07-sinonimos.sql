019ddbb14ecbd4e76496408a37a1ae7f20cdef76
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-22 11:23:30 -0500

    started queries

--- lib/s-07-sinonimos.sql
+++ lib/s-07-sinonimos.sql
@@ -43,6 +43,11 @@ grant select on status_vuelo to em_proy_invitado;
 prompt asignando permiso de lectura a em_proy_invitado para tabla historico_status_vuelo;
 grant select on historico_status_vuelo to em_proy_invitado;
 
+prompt asignando permiso de lectura para vistas v_impresion_pase_abordar y v_impresion_lista_pasajeros;
+grant select on v_impresion_lista_pasajeros to em_proy_invitado;
+grant select on v_impresion_pase_abordar to em_proy_invitado;
+grant select on v_impresion_equipaje to em_proy_invitado;
+
 -----
 prompt conectando como usuario em_proy_invitado para crear sinónimos privados;
 connect em_proy_invitado/emi;
@@ -50,33 +55,39 @@ connect em_proy_invitado/emi;
 --
 -- Synonym: LISTA_UBICACIONES
 --
-prompt creando diccionario privado para el ususario em_proy_invitado sobre tablalista_ubicaciones;
-create or replace synonym lista_ubicaciones for 
-    em_proy_admin.lista_ubicaciones;
+prompt creando sinónimo privado para el ususario em_proy_invitado sobre tablalista_ubicaciones;
+create or replace synonym ubicaciones_log for 
+    em_proy_admin.ubicaciones_log;
 
 --
 -- Synonym: AVION
 --
-prompt creando diccionario privado para el ususario em_proy_invitado sobre tabla avion;
+prompt creando sinónimo privado para el ususario em_proy_invitado sobre tabla avion;
 create or replace synonym avion for em_proy_admin.avion;
 
 --
 -- Synonym: STATUS_VUELO
 --
-prompt creando diccionario privado para el ususario em_proy_invitado sobre tabla status_vuelo;
+prompt creando sinónimo privado para el ususario em_proy_invitado sobre tabla status_vuelo;
 create or replace synonym status_vuelo for em_proy_admin.status_vuelo;
 
 --
 -- Synonym: HISTORICO_STATUS_VUELO
 --
-prompt creando diccionario privado para el ususario em_proy_invitado sobre tabla historico_status_vuelo;
+prompt creando sinónimo privado para el ususario em_proy_invitado sobre tabla historico_status_vuelo;
 create or replace  synonym historico_status_vuelo for
     em_proy_admin.historico_status_vuelo;
+
+prompt creando sinónimo privado para las vistas;
+create or replace synonym v_impresion_pase_abordar for
+    em_proy_admin.v_impresion_pase_abordar;
+create or replace synonym v_impresion_equipaje for
+    em_proy_admin.v_impresion_equipaje;
 
 -----
 prompt conectando como usuario em_proy_admin para sinónimos privados en prefijos de tablas;
 connect em_proy_admin/ema;
-/*
+
 prompt ejecutando bloque anónimo para crear sinónimos privados con prefijos;
 declare
 cursor cur_admin_tables is
@@ -86,14 +97,12 @@ cursor cur_admin_tables is
 stmt varchar2(200);
 begin
     for r in cur_admin_tables loop
-        synonym_name := 'XX_' || r.table_name;
         stmt := 'create or replace synonym ' 
             || 'XX_'
             || r.table_name
-            || ' from em_proy_admin.r.table_name';
+            || ' for em_proy_admin.r.table_name';
         execute immediate stmt;
     end loop;
 end;
 /
-*/
 show errors;
--- lib/s-10-consultas.sql
+++ lib/s-10-consultas.sql
@@ -1,3 +1,43 @@
 --@Autor(es):       Hector Espino Rojas, Néstor Martínez Ostoa
 --@Fecha creación:  15/06/2020
---@Descripción:     Script con algunas consultas a la base de datos.
+--@Descripción:     Script con consultas a la base de datos.
+
+--
+-- 1: Cantidad de pasajeros que viajaron con air-flights en enero del 2016
+--
+select count(*) as num_pasajeros
+from lista_pasajeros lp, (
+  select v.*
+  from vuelo v
+  join avion a
+  on v.id_avion = a.id_avion
+  where a.es_comercial = 1
+) as q1
+where lp.id_vuelo = q1.id_vuelo
+and q1.fecha_hora_salida in between (to_date('1/1/2016', 'dd/mm/yyyy')
+  and to_date('31/1/2016', 'dd/mm/yyyy')
+)
+order by q1.fecha_hora_salida;
+
+--
+-- 2: Aumento del 10% en el sueldo a los empleados que hayan volado en más de 100
+-- vuelos y que los vuelos no hayan sido cancelados
+--
+
+--
+-- 3: Cantidad de aeroupuertos que tengan convenio con air-flights (usando t ext)
+--
+
+--
+-- 4: Cantidad de pasajeros ausentes en vuelos del 2019 (v_impresion_lista_pasajeros)
+-- 
+
+--
+-- 5: Cuantos vuelos de carga y comercial hay en el momento (t_ubicacion)
+--
+
+--
+-- 6: Obtener la información de pasajeros para id_vuelo = 601
+--
+prompt conectando como invitado;
+connect em_proy_invitado/emi;
