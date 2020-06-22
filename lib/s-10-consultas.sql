e6f616cd8243277ed397323147d47067f355ca50
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-22 12:29:56 -0500

    started query 2

--- lib/s-10-consultas.sql
+++ lib/s-10-consultas.sql
@@ -34,8 +34,25 @@ order by lp.id_vuelo;
 
 --
 -- 2: Aumento del 10% en el sueldo a los empleados que hayan volado en más de 100
--- vuelos y que los vuelos no hayan sido cancelados
+-- vuelos y que los vuelos no hayan sido cancelados o que tenga arriba de 5000 puntos
 --
+/*
+  id_empleado | nombre | ap_paterno | ap_materno | puntos | nombre_puesto | num_vuelos
+*/
+--q1: empleados que tengan más de 100 vuelos
+--q2: q1 - vuelos cancelados ?  
+--q3: empleados que tengan arriba de 5,000
+
+--algebra relacional: q1 U q3
+select e.nombre, count(*) as num_vuelos
+from empleado e
+join tripulacion t on t.id_empleado = e.id_empleado
+group by e.nombre
+having (
+  select count(*)
+  from empleado e
+  join tripulacion t on t.id_empleado = e.id_empleado;
+) > 100;
 
 --
 -- 3: Cantidad de aeroupuertos que tengan convenio con air-flights (usando t ext)
