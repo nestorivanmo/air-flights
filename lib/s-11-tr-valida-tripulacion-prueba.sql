e15b68470ff6100723498f9e5302062f9046a0da
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-21 23:22:58 -0500

    started test for trigger

---
+++ lib/s-11-tr-valida-tripulacion-prueba.sql
@@ -0,0 +1,18 @@
+--@Autor(es):       Hector Espino Rojas, Néstor Martínez Ostoa
+--@Fecha creación:  15/06/2020
+--@Descripción:     Prueba para el trigger que para validar la cantidad de tripulantes.
+set serveroutput on;
+
+--
+-- Prueba 1 INVALIDO: tipo_vuelo: hibrido, ingresar 2 pilotos
+-- 
+
+
+--
+-- Prueba 2 INVALIDO: tipo_vuelo: hibrido, ingresar 20 tecnicos
+-- 
+
+
+--
+-- Prueba 3 INVALIDA: tipo_vuelo: carga, ingresar 1 copiloto
+-- 
