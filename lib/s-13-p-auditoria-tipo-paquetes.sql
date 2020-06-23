0458ccd83d0b7a40230027050e811296676dd45a
hectorhmx <hectorhmx55@gmail.com>
2020-06-23 13:22:52 -0500

    finished auditoria_tipo_paquetes

--- lib/s-13-p-auditoria-tipo-paquetes.sql
+++ lib/s-13-p-auditoria-tipo-paquetes.sql
@@ -4,9 +4,10 @@
 set serveroutput on;
 
 create or replace procedure sp_auditoria_tipo_paquete(
-  p_clave_paquete in varchar2
+  p_clave_paquete in varchar2,
+  p_fecha_auditoria in date
 ) is
-cursor cur_aviones sys_refcursor;
+cur_aviones SYS_REFCURSOR;
 v_es_carga_cur number;
 v_es_comercial_cur number;
 v_id_avion_cur number;
@@ -20,6 +21,33 @@ v_id_status_vuelo number;
 v_numero_vuelo varchar2(8);
 v_id_avion number;
 v_num_paquetes number;
+
+cursor cur_vuelos is 
+   select v.id_vuelo,v.fecha_hora_salida,v.fecha_hora_llegada,sv.descripcion,
+   v.id_avion,ao.nombre as aeropuerto_origen, ad.nombre as aeropuerto_destino,
+   v.numero_vuelo,count(*) as num_paquetes 
+  from vuelo v
+  join lista_paquetes lp
+  on v.id_vuelo = lp.id_vuelo
+  join avion av
+  on v.id_avion = av.id_avion
+  join paquete pa
+  on pa.id_paquete = lp.id_paquete
+  join aeropuerto ao
+  on v.id_aeropuerto_origen = ao.id_aeropuerto
+  join aeropuerto ad
+  on v.id_aeropuerto_destino = ad.id_aeropuerto
+  join status_vuelo sv
+  on sv.id_status_vuelo = v.id_status_vuelo
+  where av.es_carga = 1 
+  and to_char(v.fecha_hora_salida,'YYYY/MM') = 
+  to_char(p_fecha_auditoria,'YYYY/MM') 
+  and pa.id_tipo_paquete = (
+    select id_tipo_paquete from tipo_paquete where clave = p_clave_paquete
+  )
+  group by v.id_vuelo,v.fecha_hora_salida,v.fecha_hora_llegada,sv.descripcion,
+   v.id_avion,ao.nombre, ad.nombre,v.numero_vuelo
+  order by num_paquetes;
 begin
   /*
     id_vuelo | origen | destino | hora_salida | hora_llegada | id_avion | num_paquetes
@@ -27,51 +55,22 @@ begin
   --obteniendo todos los aviones de carga (incluyendo a los híbridos)
   v_es_carga_cur := 1;
   v_es_comercial_cur := 0;
-  cur_aviones := fx_obtener_cursor_tipo_aviones(v_es_carga_cur, v_es_comercial_cur);
-  loop
-  fetch cur_aviones into v_id_avion_cur, v_es_carga_cur, v_es_comercial_cur
-    exit when cur_aviones%notfound;
-    select v.id_vuelo, v.id_aeropuerto_origen, v.id_aeropuerto_destino, v.fecha_hora_salida,
-      v.fecha_hora_llegada, v.numero_vuelo, v.id_status_vuelo, v.id_avion, count(*) as
-        num_paquetes
-    into v_id_vuelo, v_id_aeropuerto_origen, v_id_aeropureto_destino,
-      v_fecha_hora_salida, v_fecha_hora_llegada, v_numero_vuelo, v_id_status_vuelo, 
-      v_id_avion, v_num_paquetes
-    from vuelo v
-    where id_avion = v_id_avion_cur
-    join lista_paquetes lp
-    on lp.id_vuelo = v.id_vuelo
-    group by v.id_vuelo, v.id_aeropuerto_origen, v.id_aeropuerto_destino, 
-      v.fecha_hora_salida, v.fecha_hora_llegada, v.numero_vuelo, v.id_status_vuelo
-      v.id_avion
-    having lp.id_paquete = (
-      select id_paquete
-      from paquete
-      where id_tipo_paquete = (
-        select id_tipo_paquete
-        from tipo_paquete
-        where clave = p_clave_paquete
-      )
-    );
-    dbms_output.put_line(v_id_vuelo 
-      || '|'
-      || v_id_aeropuerto_origen
-      || '|'
-      || v_id_aeropureto_destino
-      || '|'
-      || v_fecha_hora_salida
-      || '|'
-      || v_fecha_hora_llegada
-      || '|'
-      || v_numero_vuelo
-      || '|'
-      || v_id_status_vuelo
-      || '|'
-      || v_id_avion
-      || '|'
-      || v_num_paquetes
-    );
+  cur_aviones := fx_obtener_cursor_tipo_aviones(v_es_carga_cur, 
+  v_es_comercial_cur);
+  
+  for r in cur_vuelos loop
+
+    dbms_output.put_line(r.id_vuelo ||','|| r.numero_vuelo
+    ||','|| r.aeropuerto_origen 
+    ||','|| r.aeropuerto_destino
+    ||','|| to_char(r.fecha_hora_salida,'YYYY/MM/DD') 
+    ||','|| r.descripcion 
+    ||','|| r.id_avion  
+    ||','||r.num_paquetes);
+
   end loop;
+ 
+  
 end;
 /
 show errors;
--- lib/s-14-p-auditoria-tipo-paquetes-prueba.sql
+++ lib/s-14-p-auditoria-tipo-paquetes-prueba.sql
@@ -5,9 +5,11 @@ set serveroutput on;
 
 declare
 v_clave_tipo_paquete varchar2(5);
+v_fecha_auditoria date;
 begin
   v_clave_tipo_paquete := 'ALIM';
-  sp_auditoria_tipo_paquete(v_clave_tipo_paquete);
+  v_fecha_auditoria := to_date('06/2018','MM/YYYY');
+  sp_auditoria_tipo_paquete(v_clave_tipo_paquete,v_fecha_auditoria);
 end;
 /
 show errors;
