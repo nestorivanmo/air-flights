8b6ce4f1b682c30cf1df34243940a8a139bcd217
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-23 15:08:09 -0500

    fixed test

--- lib/s-12-tr-registro-ubicaciones-prueba.sql
+++ lib/s-12-tr-registro-ubicaciones-prueba.sql
@@ -11,11 +11,12 @@ Casos de prueba:
 
 -- Prueba 1: update
 declare
-v_insert_stmt varchar2(500);
+v_query varchar2(500);
+v_id_ubicacion := 1;
 v_latitud number := 31.12194021;
 v_longitud number := 107.518547;
 v_fecha_hora date := to_date('23/06/2020 11:11:11','dd/mm/yyyy HH24:MI:SS');
-v_id_vuelo number := 50000;
+v_id_vuelo number := 3666;
 --
 v_counter_old number;
 v_counter_new number;
@@ -24,16 +25,15 @@ begin
   select count(*)
   into v_counter_old
   from ubicaciones_log
-  where id_vuelo = v_id_vuelo and fecha_hora = v_fecha_hora and latitud = v_latitud
-  and longitud = v_longitud;
-  v_insert_stmt := ' insert into ubicaciones_log(id_ubicaciones_log, id_vuelo,
-      latitud, longitud, fecha_hora
-    ) values (
-      ubicaciones_log_seq.nextval, :id_vuelo, :latitud, :longitud, :fecha_hora
-    )
+  where id_vuelo = v_id_vuelo;
+  v_query := 'update t_ubicacion
+    set latitud = :latitud,
+    set longitud = :longitud,
+    set fecha_hora = :fecha_hora
+    where id_vuelo = :id_vuelo and id_ubicacion = :id_ubicacion
   ';
-  execute immediate v_insert_stmt using v_id_vuelo, v_latitud, v_longitud, 
-    v_fecha_hora;
+  execute immediate v_query using v_latitud, v_longitud, v_fecha_hora, 
+    v_id_vuelo, v_id_ubicacion;
   select count(*)
   into v_counter_new
   from ubicaciones_log
