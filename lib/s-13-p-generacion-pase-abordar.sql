f034125347b96ef2eb6adae2fbe671d497c80e34
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-22 23:23:32 -0500

    updated sp 13

--- lib/s-13-p-generacion-pase-abordar.sql
+++ lib/s-13-p-generacion-pase-abordar.sql
@@ -3,22 +3,22 @@
 --@Descripción:     Script que simula la generación de pases de abordar. 
 set serverouput on;
 
-prompt compilando funcion;
-start s-15-fx-id-pasajero.sql
-
 create or replace procedure sp_genera_pase_abordar (
-  p_nombre is varchar2, p_apellido_paterno is varchar2, p_apellido_materno 
-  is varchar2, p_email is varchar2, p_fecha_nacimiento is date, 
-  p_curp is varchar2, p_id_origen is number, p_id_destino is number, 
-  p_fecha_hora_salida is number, p_num_vuelo is
-  varchar2
+  p_nombre in varchar2, p_apellido_paterno in varchar2, p_apellido_materno 
+   in varchar2, p_email in varchar2, p_fecha_nacimiento in date, 
+  p_curp in varchar2, p_id_origen in number, p_id_destino in number, 
+  p_fecha_hora_salida in number, p_num_vuelo in varchar2, p_atencion_especial
+  in varchar2
 ) is
 v_pasajero_presente number;
 v_insert_stmt varchar2(500);
 v_insert_pase_abordar_stmt varchar2(500);
 v_id_pasajero number;
 v_folio varchar2(10);
+v_id_vuelo number;
+v_asiento varchar2(5);
 begin
+  --obteniendo el id_pasajero
   v_pasajero_presente := fx_checa_pasajero(p_curp);
   if v_pasajero_presente = 0 then
     --insertando al pasajero en la BD en caso de que no exista
@@ -47,7 +47,20 @@ begin
   execute immediate v_insert_pase_abordar_stmt using v_folio, sysdate, 
     v_id_pasajero;
   --insertando al usuario en la lista de pasajeros del vuelo indicado
-  
+  v_id_vuelo := fx_checa_vuelo(p_id_origen, p_id_destino, p_fecha_hora_salida,
+    p_num_vuelo);
+  --insertando en lista pasajeros
+  v_insert_stmt := 'insert into lista_pasajeros(
+      id_lista_pasajeros, id_vuelo, id_pase_abordar, asiento, pasajero_presente, 
+      atencion_espcial
+    ) values (
+      lista_pasajeros_seq.nextval, :id_vuelo, :id_pase_abordar, :asiento, 
+      :pasajero_presente, :atencion_especial
+    )';
+  --usamos por defecto 0 porque no sabemos si el pasajero llegará a la sala de abordar
+  v_asiento := dbms_random.string('U',1) || '-' || trunc(dbms_random.value(1,10), 0); --U: random upper case letter
+  execute immediate v_insert_stmt using v_id_vuelo, pase_abordar_seq.currval,
+    v_asiento, 0, p_atencion_especial;
 end;
 /
 show errors;
