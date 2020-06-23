546349e52233ea6ba8f91b08c3c087092a2db120
hectorhmx <hectorhmx55@gmail.com>
2020-06-23 17:23:00 -0500

    Final comit

--- lib/s-00-main.sql
+++ lib/s-00-main.sql
@@ -26,12 +26,17 @@ start s-09-carga-inicial.sql
 
 /*
 start s-10-consultas.sql
+connect em_proy_admin/ema;
 start s-15-fx-consulta-id-pasajero.sql
 start s-15-fx-consulta-vuelo.sql
 start s-15-fx-obten-aviones.sql
 start s-16-fx-consulta-id-pasajero-prueba.sql
 start s-16-fx-consulta-vuelo-prueba.sql
 start s-16-fx-obten-aviones-prueba.sql
+
+start s-11-tr-registro-ubicaciones.sql
+start s-12-tr-registro-ubicaciones-prueba.sql
+
 
 start s-13-p-auditoria-tipo-paquetes.sql
 start s-13-p-generacion-pase-abordar.sql
@@ -40,10 +45,7 @@ start s-14-p-generacion-pase-abordar-prueba.sql
 
 
 start s-11-tr-pase-abordar.sql
-start s-11-tr-registro-ubicaciones.sql
 start s-11-tr-valida-tripulacion.sql
 
-
-start s-12-tr-registro-ubicaciones-prueba.sql
 
 */
