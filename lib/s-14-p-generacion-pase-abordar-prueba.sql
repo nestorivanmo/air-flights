c24593d1e58fb6c191a251a019ddf48b66ddcf18
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-22 23:31:15 -0500

    started test of sp 13

---
+++ lib/s-14-p-generacion-pase-abordar-prueba.sql
@@ -0,0 +1,12 @@
+--@Autor(es):       Hector Espino Rojas, Néstor Martínez Ostoa
+--@Fecha creación:  22/06/2020
+--@Descripción:     Prueba para el procedimiento que simula la generación de pases de abordar. 
+set serverouput on;
+
+declare
+
+begin
+  
+end;
+/
+show errors;
