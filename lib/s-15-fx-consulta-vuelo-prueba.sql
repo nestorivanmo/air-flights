53b0e89e8ae1c3152621e1a68c3d17afc1848588
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-22 22:07:51 -0500

    added fx and tests

---
+++ lib/s-15-fx-consulta-vuelo-prueba.sql
@@ -0,0 +1,52 @@
+--@Autor(es):       Hector Espino Rojas, Néstor Martínez Ostoa
+--@Fecha creación:  22/06/2020
+--@Descripción:     Prueba de la función para determinar si un vuelo existe.
+set serverouput on;
+
+declare 
+v_id_aeropuerto_origen number; 
+v_id_aeropuerto_destino number;
+v_fecha_hora_salida date;
+v_numero_vuelo varchar2;
+v_id_vuelo number;
+begin
+  dbms_output.put_line('comenzando prueba 1');
+  v_id_aeropuerto_origen := 996;
+  v_id_aeropuerto_destino := 592;
+  v_fecha_hora_salida := to_date('2016/11/28', 'YYYY/MM/DD');
+  v_numero_vuelo := 'ZH-9471';
+  v_id_vuelo := fx_checa_vuelo(v_id_aeropuerto_origen, v_id_aeropuerto_destino,
+    v_fecha_hora_salida, v_numero_vuelo);
+  if v_id_vuelo = 1 then 
+    dbms_output.put_line('Prueba válida');
+  else
+    dbms_output.put_line('Prueba fallida, id_vuelo incorrecto');
+  end if;
+exception
+  when 20004 then
+    dbms_output.put_line('Prueba fallida, no debería lanzar excepción');
+end;
+/
+show errors;
+
+declare 
+v_id_aeropuerto_origen number; 
+v_id_aeropuerto_destino number;
+v_fecha_hora_salida date;
+v_numero_vuelo varchar2;
+v_id_vuelo number;
+begin
+  dbms_output.put_line('comenzando prueba 2');
+  v_id_aeropuerto_origen := 10;
+  v_id_aeropuerto_destino := 10;
+  v_fecha_hora_salida := to_date('2016/10/10 06:08:30', 'YYYY/MM/DD HH24:MI:SS');
+  v_numero_vuelo := 'AB-1345';
+  v_id_vuelo := fx_checa_vuelo(v_id_aeropuerto_origen, v_id_aeropuerto_destino,
+    v_fecha_hora_salida, v_numero_vuelo);
+  dbms_output.put_line('Prueba fallida, no debería regresar id_vuelo');
+exception
+  when 20004 then
+    dbms_output.put_line('Prueba válida');
+end;
+/
+show errors;
---
+++ lib/s-15-fx-consulta-vuelo.sql
@@ -0,0 +1,26 @@
+--@Autor(es):       Hector Espino Rojas, Néstor Martínez Ostoa
+--@Fecha creación:  22/06/2020
+--@Descripción:     Función para determinar si un vuelo existe.
+set serverouput on;
+
+crete or replace function fx_checa_vuelo( 
+  p_id_aeropuerto_origen in number, p_id_aeropuerto_destino in number,
+  p_fecha_hora_salida in date, p_numero_vuelo in varchar2
+) return number is
+v_num number;
+v_id_vuelo number;
+begin
+  select id_vuelo
+  into v_id_vuelo
+  from vuelo
+  where id_aeropuerto_origen = p_id_aeropuerto_origen
+  and id_aeropuerto_destino = p_id_aeropuerto_destino
+  and fecha_hora_salida = p_fecha_hora_salida
+  and numero_vuelo = p_numero_vuelo;
+  return id_vuelo;
+exception
+  when NO_DATA_FOUND then
+    raise_application_error(20004, 'El vuelo no existe para los datos proporcionados');
+end;
+/ 
+show errors;
